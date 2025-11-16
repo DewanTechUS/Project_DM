e6158107af228fd1e3c9b7656d1d14ece11a3a1c
DewanTechUS <rockyit10@gmail.com>
2025-11-15 23:55:48 -0500

    feat add

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ const child = document.querySelector('.Child');
 grandparent.addEventListener('click', (e) => { // e is the event object // the event that just happened // click
     console.log('Grandparent clicked');
 });
+
+parent.addEventListener('click', (e) => {
+    console.log('Parent clicked');
+    // e.stopPropagation(); // Stop the event from bubbling up to grandparent
+});
+child.addEventListener('click', (e) => {
+    console.log('Child clicked');
+    // e.stopPropagation(); // Stop the event from bubbling up to parent and grandparent
+}); 
+
